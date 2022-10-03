7f52f02e719d52ac80b863e11fedb0ea0f434ba5
prettypain <jkungjk1123@gmail.com>
2022-10-03 14:17:06 +0900

    로딩 이미지 추가및 명언 업데이트

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const api_key = config.apikey;
 $.ajax({
 	url : "https://api.winsub.kr/hangang/?key=" + api_key,
@@ -72,12 +73,7 @@ function copy() {
 	alert_box("온도 복사 완료!", "success", 2000);
 }
 
-$(window).on('load', function(){
-	$('#load').hide();
-	$( document ).ready( function() {
-		$( 'div.i' ).fadeIn( 1800 );
-	} );
-})
+
 /* pc 에 따라 모바일에 따라 구분지어 각 화면을 비춘다.
 var filter = "win16|win32|win64|mac|macintel";
 if ( navigator.platform )
@@ -90,15 +86,27 @@ if ( navigator.platform )
 	}
 }
 */
-var List = ["노력은 자신을 배신하지 않지. 꿈을 배신하는 건 있지만.",
+
+//명언 배열
+var List = [
+	"The hatred of the two families could not stop their love. ― Romeo and Juliet", //로미오와 줄리엣
+	"you see clearly only with your heart. Nothing important can be seen with your eyes. ― The Little Prince", //어린 왕자
+	'what is in men? &nbsp what is not given men? &nbsp what men live by? ― tolstoy three questions', //톨스토이의 미하일이 배워야할 과제 3가지
+	"What does not destroy me, makes me stronger. ― Friedrich Nietzsche", //프리드리히 니체
+	"나를 죽이지 못하는 고통은, 나를 더 강하게 해줄 뿐이다. ― 프리드리히 니체", //우상의 황혼
+	"<a style='color:black; font-size:20pt' href='https://www.kookmin.ac.kr/comm/menu/user/d48d445aa00b36a21e59610221023b0d/content/index.do'>以校爲家 事必歸正</a>",
+
+]
+/* //이전 명언
+"노력은 자신을 배신하지 않지. 꿈을 배신하는 건 있지만.",
 	"씁쓸한 인생, 커피 정도는 달아도 괜찮겠지.",
 	"과거를 떠올리면 후회 때문에 죽고 싶고 미래를 생각하면 불안 때문에 우울하니까,<br/> 소거법으로 현재는 행복하다고 할 수 있지.",
 	"문제는 문제로 삼지 않는 한 문제가 되지 않아",
 	"사회가 내게 가혹하니 나만이라도 나에게 관대해야겠다.",
 	"세계를 좁혀 간다는 건 틀림없이 어른에 가까워져 간다는 것이다. 수많은 선택지를 좁혀 가며,<br/> 가능성을 죽여 가며, 더욱 확실한 미래상을 만들어간다.",
-]
 
-
+*/
+//background img 위치 배열
 var arr = new Array();
 arr[0] = "./hanRiver_image/wallpaper_0.png";
 arr[1] = "./hanRiver_image/wallpaper_1.jpg";
@@ -133,3 +141,10 @@ function print_time(){
 	data.innerHTML = parseInt(elapsedSec);
 	setTimeout("print_time()",1000);
 }
+
+$(window).on('load', function(){
+	$('#load').hide();
+	$( document ).ready( function() {
+		$( 'div.i' ).fadeIn( 1800 );
+	} );
+})
